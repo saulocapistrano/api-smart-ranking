1ff51da0feb64b2929cf6f5ad8464a8287c8b45f
saulocapistrano <saulocapistrano@gmail.com>
2025-02-04 10:32:03 -0300

    feat: add new modules or configurations in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PlayersModule } from './players/player.module';
+
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://saulo:Woch3WIrwB6543wQ@c-smart-ranking.913rb.mongodb.net/smartranking?retryWrites=true&w=majority&appName=c-smart-ranking',
+    ),
+    PlayersModule,
+  ],
   controllers: [],
   providers: [],
 })
