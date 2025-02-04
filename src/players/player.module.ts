00fa57febab78f33fe7ce9e9c612cf213fda8d11
saulocapistrano <saulocapistrano@gmail.com>
2025-02-04 10:12:38 -0300

    feat(players): configurar módulo de Player com Mongoose e Controller

---
+++ src/players/player.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { PlayersController } from './player.controller';
+import { PlayersService } from './player.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PlayerSchema } from './schemas/player.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Player', schema: PlayerSchema }]),
+  ],
+  controllers: [PlayersController],
+  providers: [PlayersService],
+})
+export class PlayersModule {}
