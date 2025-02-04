709c48ed8ef8a51e9993897c8066b885f015642b
saulocapistrano <saulocapistrano@gmail.com>
2025-02-04 10:25:04 -0300

    feat(players): define Mongoose schema for Player with timestamps and unique email

---
+++ src/players/schemas/player.schema.ts
@@ -0,0 +1,13 @@
+import * as mongoose from 'mongoose';
+
+export const PlayerSchema = new mongoose.Schema(
+  {
+    phoneNumber: { type: String, unique: false },
+    email: { type: String, unique: true },
+    name: String,
+    ranking: String,
+    rankingPosition: Number,
+    urlImagePlayer: String,
+  },
+  { timestamps: true, collection: 'players' },
+);
