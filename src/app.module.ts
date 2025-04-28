a47b32ceaa6d17d863c20859c47fa1c1f0c68f19
saulocapistrano <saulocapistrano@gmail.com>
2025-04-28 08:38:17 -0300

    feat: add CategoriesModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './players/player.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import { PlayersModule } from './players/player.module';
       'mongodb+srv://saulo:Woch3WIrwB6543wQ@c-smart-ranking.913rb.mongodb.net/smartranking?retryWrites=true&w=majority&appName=c-smart-ranking',
     ),
     PlayersModule,
+    CategoriesModule,
   ],
   controllers: [],
   providers: [],
