89b22d9f4610a3376e944548740dd93923ee8f20
saulocapistrano <saulocapistrano@gmail.com>
2025-04-28 11:16:21 -0300

    feat: add validation decorators for phoneNumber, email, and name in CreatePlayerDTO

--- src/players/dtos/create-player.dto.ts
+++ src/players/dtos/create-player.dto.ts
@@ -1,5 +1,10 @@
+import { IsEmail, IsString } from 'class-validator';
+
 export class CreatePlayerDTO {
+  @IsString()
   readonly phoneNumber: string;
+  @IsEmail()
   readonly email: string;
+  @IsString()
   name: string;
 }
