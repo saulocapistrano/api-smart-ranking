555c64e6d3252dc9cf54c3d4c08e45ad12596352
saulocapistrano <saulocapistrano@gmail.com>
2025-04-28 15:22:38 -0300

    feat: import ChallengesModule in AppModule for challenge management

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './players/player.module';
 import { CategoriesModule } from './categories/categories.module';
+import { ChallengesModule } from './categories/challenges/challenge.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { CategoriesModule } from './categories/categories.module';
     ),
     PlayersModule,
     CategoriesModule,
+    ChallengesModule,
   ],
   controllers: [],
   providers: [],
